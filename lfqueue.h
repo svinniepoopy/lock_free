620fcc2f80c18a8f1abb3703e4d5c65c9e2c9b79
svinniepoopy <akhil.jaggarwal@gmail.com>
2019-01-25 23:46:48 -0500

    added tests

--- lfqueue.h
+++ lfqueue.h
@@ -7,101 +7,109 @@
  * concurrent queue algorithms. PODC 96.
  */
 
-namespace {
-    template<typename T>
-	bool compare_and_swap(T** owner, const T* expected, T** desired) {
-	    if (*owner == expected) {
-		*owner = *desired;
-		return false;
-	    }
-	    return true;
-	}
+namespace lfatomic {
+  template<typename T>
+    bool compare_and_swap(T** owner, const T* expected, T** desired) {
+      if (*owner == *desired) {
+	return true;
+      }
+      if (*owner == expected) {
+	*owner = *desired;
+	return true;
+      }
+      return false;
+    }
 }; // namespace
+
+using namespace lfatomic;
 
 template<typename T>
 class lfqueue {
-    public:
-	struct node {
-	    node():
-		pos{0},
-		next{nullptr}
-	    {}
-	    node(const T& v, int p=0):
-		val{v},
-		pos{p},
-		next{nullptr}
-	    {}
-	    T val;
-	    int pos;
-	    node* next;
-	};
+  public:
+    struct node {
+      node():
+	pos{0},
+	next{nullptr}
+      {}
+      node(const T& v, int p=0):
+	val{v},
+	pos{p},
+	next{nullptr}
+      {}
+      T val;
+      int pos;
+      node* next;
+    };
 
-	lfqueue():
-	    head{new node} {
-		tail = head;
-		tail->next = head->next = nullptr;
+    lfqueue() {
+	head = new node;
+	tail = head; 
+	tail->next = nullptr;
+      }
+
+    ~lfqueue() {
+      node* tmp;
+      if (head) {
+	tmp = head->next;
+	delete head;
+	head = tmp;
+      }
+      head = 0;
+      tail = 0;
+    }    
+
+    const node *const front() const {
+      return head;
+    }
+
+    bool pop() {
+      node* hd=nullptr;
+      while (1) {
+	hd = head;
+	node* tl = tail;
+	node* nxt = head->next;
+	if (hd == head) {
+	  if (head == tail) {
+	    if (nxt == nullptr) {
+	      return false;
 	    }
-
-	~lfqueue() {
-	    node* tmp;
-	    if (head) {
-		tmp = head->next;
-		delete head;
-		head = tmp;
+	    compare_and_swap(&tail, tl, &nxt);
+	  } else {
+	    node* _nxt_ = nxt;
+	    if (compare_and_swap(&head, hd, &nxt)) {
+	      break;
 	    }
-	    head = 0;
-	    tail = 0;
-	}    
-
-	const node* front() const {
-	    return head;
+	  }
 	}
-	bool pop() {
-	    node* hd;
-	    while (1) {
-		hd = head;
-		node* tl = tail;
-		node* nxt = head->next;
-		if (hd == head) {
-		    if (head == tail) {
-			if (nxt == nullptr) {
-			    return false;
-			}
-			compare_and_swap(&tail, tl, &nxt);
-		    } else {
-			node* _nxt_ = nxt;
-			if (!compare_and_swap(&head, hd, &nxt)) {
-			    break;
-			}
-		    }
-		}
-	    }
-	    delete hd;
-	    return true;
-	}
+      }
+      if (hd) {
+	delete hd;
+      }
+      return true;
+    }
 
-	void push(const T& val) {
-	    const node* const front_node = front();
-	    node* newnod = new node{val,front_node->pos+1};
-	    newnod->next = nullptr;
-	    node* tl;
-	    while (1) {
-		tl = tail;
-		node* nxt = tail->next;
-		if (tl == tail) { 
-		    if (nxt == nullptr) {	// tail is pointing to the last node
-			if (!compare_and_swap(&tail->next, nxt, &newnod)) {
-			    break;
-			}
-		    } else {	// tail was not pointing to the last node
-			compare_and_swap(&tail, tl, &nxt);  
-		    }
-		}
+    void push(const T& val) {
+      const node* const front_node = front();
+      node* newnod = new node{val,front_node->pos+1};
+      newnod->next = nullptr;
+      node* tl;
+      while (1) {
+	tl = tail;
+	node* nxt = tail->next;
+	if (tl == tail) { 
+	  if (nxt == nullptr) {	// tail is pointing to the last node
+	    if (compare_and_swap(&tail->next, nxt, &newnod)) {
+	      break;
 	    }
-	    compare_and_swap(&tail, tl, &newnod);
+	  } else {	// tail was not pointing to the last node
+	    compare_and_swap(&tail, tl, &nxt);  
+	  }
 	}
-    private:
-	node* head, *tail;
+      }
+      compare_and_swap(&tail, tl, &newnod);
+    }
+  private:
+    node* head, *tail;
 };
 
 #endif
--- lfqueue_test.cpp
+++ lfqueue_test.cpp
@@ -2,43 +2,68 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <cassert>
 
 template<typename T>
 void run(lfqueue<T>& q, int nthreaditems) {
-    std::cout << "ntitems: " << nthreaditems << '\n';
   for (int i=1;i<=nthreaditems;++i) {
-    std::cout << "push " << i << '\n';
     q.push(i);
   }
 }
 
-bool test_queue_threaded(int nthreads, int nitems) {
+bool test_queue() {
+  int nthreads=10000,nitems=100;
   lfqueue<int> q;
   std::vector<std::thread> threads;
-  std::cout << "ntitems: " << nthreads << '\n';
   for (int i=1;i<=nthreads;++i) {
-    std::cout << "running thread: " << i << '\n';
     threads.push_back(std::thread{[&]{
-	  run<int>(q, i);
+	  run<int>(q, nitems);
 	  }});
   }
   for (auto& t: threads) {
     t.join();
-  } 
+  }
   int nqitems=0;
   while (q.pop()) {
     ++nqitems;
   }
-  return (nqitems == nitems*(nitems+1)/2);
+
+  return (nqitems == nthreads*nitems); 
+}
+
+using namespace lfatomic;
+int test_CAS() {
+  using node = lfqueue<int>::node;
+  node* owner = new node(42);
+  node* expected = owner;
+  node* desired = nullptr; 
+  bool is_swapped = compare_and_swap(&owner, expected, &desired);
+  assert(is_swapped);
+  assert(owner == desired);
+  delete expected;
+  //
+  owner = new node(1);
+  node* _owner_ = owner;
+  expected = new node(3);
+  node* _expected_ = expected;
+  desired = new node(5);;
+  node* _desired_ = desired;
+  is_swapped = compare_and_swap(&owner, expected, &desired);
+  assert(!is_swapped);
+  assert(owner == _owner_);
+  assert(expected == _expected_);
+  assert(desired == _desired_);
+  delete owner;
+  delete expected;
+  delete desired;
+  return 0;
 }
 
 int main(int argc, char** argv) {
-  if (argc < 3) {
-    std::cerr << "Usage: qtest nthreads nitems\n";
+  if (test_CAS() < 0) {
+    std::cerr << "test_CAS OK!\n";
     return 1;
   }
-  if (test_queue_threaded(1, 1)) {
-    std::cout << "OK!\n";
-  }
+  assert(test_queue());
   return 0;
 }
